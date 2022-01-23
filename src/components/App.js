4a3746c7bcba0a0c40e3fdcabc49925a806f3eb4
Hee <unghee66@gmail.com>
2022-01-24 00:02:43 +0900

    2.0 Make Login Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
+import { authService } from "fbInstance";
+import { useState } from "react";
+import Router from "components/Router";
+
 function App() {
-  return null;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <Router isLoggedIn={isLoggedIn} />
+      <footer>
+        &copy; {new Date().getFullYear()} Nwitter
+      </footer>
+    </>
+  );
 }
 
 export default App;
